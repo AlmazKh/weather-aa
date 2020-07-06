4b72fd0ba1ac8b2b26ae5089e60a39974af337db
AlmazKh <hamedzhanovalmaz@gmail.com>
2020-07-06 12:44:29 +0300

    init project

---
+++ app/src/main/java/com/almaz/weather_aa/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.almaz.weather_aa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
