8ccb6d2d018519d667c6eb38abe7d3377fc9d381
adelina609 <adelinarid609@gmail.com>
2020-07-08 16:21:35 +0300

    add adapter, date mapper

---
+++ app/src/main/java/com/almaz/weather_aa/ui/weather/DailyWeatherAdapter.kt
@@ -0,0 +1,58 @@
+package com.almaz.weather_aa.ui.weather
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.almaz.weather_aa.R
+import com.almaz.weather_aa.core.model.DailyWeather
+import com.almaz.weather_aa.utils.DateTimeMapper
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_weather_daily.view.*
+
+class DailyWeatherAdapter(
+    private val weatherData: List<DailyWeather>
+) : ListAdapter<DailyWeather, DailyWeatherAdapter.DailyWeatherHolder>(DailyWeatherDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, type: Int): DailyWeatherHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_weather_daily, parent, false)
+        return DailyWeatherHolder(view)
+    }
+
+    override fun getItemCount(): Int = weatherData.size
+
+    override fun onBindViewHolder(holder: DailyWeatherHolder, position: Int) {
+        holder.bind(weatherData[position])
+    }
+
+    override fun submitList(list: List<DailyWeather>?) {
+        super.submitList(if (list != null) ArrayList(list) else null)
+    }
+
+    class DailyWeatherDiffCallback : DiffUtil.ItemCallback<DailyWeather>() {
+        override fun areItemsTheSame(oldItem: DailyWeather, newItem: DailyWeather): Boolean =
+            oldItem.timestampUtc == newItem.timestampUtc
+
+        override fun areContentsTheSame(oldItem: DailyWeather, newItem: DailyWeather): Boolean =
+            oldItem == newItem
+
+    }
+
+    class DailyWeatherHolder(override val containerView: View) :
+        RecyclerView.ViewHolder(containerView),
+        LayoutContainer {
+        fun bind(weatherData: DailyWeather) {
+            containerView.tv_day_by_week.text = DateTimeMapper.mapToDayOfWeek(weatherData.datetime)
+            containerView.tv_day_by_month.text =
+                DateTimeMapper.mapToDayOfMonth(weatherData.datetime)
+            containerView.tv_max_temp.text = weatherData.appMaxTemp.toString()
+            containerView.tv_min_temp.text = weatherData.appMinTemp.toString()
+
+            //TODO разные иконки
+            containerView.iv_weather_icon.setImageResource(R.drawable.ic_sunny)
+        }
+    }
+}
