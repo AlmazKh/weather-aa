3771b9b7e828694fcac975b902d8d696bf55d7ab
adelina609 <adelinarid609@gmail.com>
2020-07-10 00:35:27 +0300

    add logger interceptor

--- app/src/main/java/com/almaz/weather_aa/di/netModule.kt
+++ app/src/main/java/com/almaz/weather_aa/di/netModule.kt
@@ -2,6 +2,8 @@ package com.almaz.weather_aa.di
 
 import com.almaz.weather_aa.data.WeatherService
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.kodein.di.Kodein
 import org.kodein.di.generic.bind
 import org.kodein.di.generic.instance
@@ -13,12 +15,27 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 fun netModule() = Kodein.Module("netModule") {
     constant("baseUrl") with "http://api.weatherbit.io/v2.0/"
-    bind<Retrofit>() with singleton { provideRetrofit(instance("baseUrl")) }
+    bind<HttpLoggingInterceptor>() with singleton { provideLogger() }
+    bind<OkHttpClient>() with singleton { provideOkHttp(instance()) }
+    bind<Retrofit>() with singleton { provideRetrofit(instance("baseUrl"), instance()) }
     bind<WeatherService>() with singleton { instance<Retrofit>().create(WeatherService::class.java) }
 }
 
-private fun provideRetrofit(baseUrl: String): Retrofit = Retrofit.Builder()
-    .baseUrl(baseUrl)
-    .addConverterFactory(GsonConverterFactory.create(GsonBuilder().setLenient().create()))
-    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-    .build()
+private fun provideRetrofit(baseUrl: String, okHttpClient: OkHttpClient): Retrofit =
+    Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create(GsonBuilder().setLenient().create()))
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .build()
+
+private fun provideOkHttp(interceptor: HttpLoggingInterceptor): OkHttpClient =
+    OkHttpClient.Builder()
+        .addInterceptor(interceptor)
+        .build()
+
+private fun provideLogger() : HttpLoggingInterceptor {
+    val interceptor = HttpLoggingInterceptor()
+    interceptor.level = HttpLoggingInterceptor.Level.BODY
+    return interceptor
+}
